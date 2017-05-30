1d21c0188347fa70ff44b962faafd2e3debcc1a0
Greg Werner <werner.greg@gmail.com>
2017-05-30 14:42:06 -0400

    Iss28/tensor (#12)
    * bump dependency version and add tensorflow
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * build base/minimal
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * attempt golang 1.8
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * use runner from core server
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * remove go get
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * bump notebook to 4.4.x
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * disabling builds with travis
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * fix merge conflicts
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * iframe support
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>
    
    * bump tensor
    
    Signed-off-by: Greg Werner <werner.greg@gmail.com>

--- base-notebook/jupyter_notebook_config.py
+++ base-notebook/jupyter_notebook_config.py
@@ -8,3 +8,4 @@ c = get_config()
 c.NotebookApp.ip = '*'
 c.NotebookApp.port = 8888
 c.NotebookApp.open_browser = False
+c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "child-src * " } }
