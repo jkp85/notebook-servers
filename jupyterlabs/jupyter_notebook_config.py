af3d7175c4f92ba8008daf77112b9f0788e1887d
Greg Werner <werner.greg@gmail.com>
2017-09-08 15:43:44 -0400

    Revert "Update notebook server images"

---
+++ jupyterlabs/jupyter_notebook_config.py
@@ -0,0 +1,14 @@
+from jupyter_core.paths import jupyter_data_dir
+import subprocess
+import os
+import errno
+import stat
+
+c = get_config()
+c.NotebookApp.ip = '*'
+c.NotebookApp.port = 8888
+c.NotebookApp.open_browser = False
+c.NotebookApp.token = ''
+c.NotebookApp.password = ''
+c.NotebookApp.tornado_settings = { 'headers': { 'Content-Security-Policy': "child-src * " } }
+c.NotebookApp.default_url = '/lab'
